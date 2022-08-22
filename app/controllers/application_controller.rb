4f0818f6123eaef1b86b56d3c5c34e777560421d
Francois Jarry <francoisjarry1@gmail.com>
2022-08-22 17:52:00 +0200

    specialties-controller first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :description, :gender, :city])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthdate, :description, :gender, :city])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[first_name last_name birthdate description gender city])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[first_name last_name birthdate description gender city])
   end
 end
