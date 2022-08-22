3cad7ba757cd57b67d5322cf23e22c2bcd441a58
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2022-08-22 18:08:07 +0200

    seed ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "cleaning database"
+
+Reservation.destroy_all
+Specialty.destroy_all
+User.destroy_all
+
+puts "creating test@test.test and azerty login"
+
+User.create!(email: "test@test.test",
+             password: "azerty",
+             first_name: "Jean-Eude",
+             last_name: "BG",
+             birthdate: DateTime.now - 25.years,
+             description: "Verrat de viande à chien de crisse de maudit de saint-cimonaque de sacréfice de crucifix d'astie de cochonnerie de saint-sacrament de purée de sacristi d'estique d'étole",
+             gender: "M",
+             city: "Paris")
+
+puts "Fake it until you make it"
+
+counter = 0
+50.times do
+  user_resa = User.all.sample
+  puts "creating user n°#{counter + 1}"
+  user = User.create!(email: Faker::Internet.email,
+               password: "abcdefg",
+               first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name,
+               birthdate: DateTime.now - (19..90).to_a.sample.years,
+               description: Faker::Lorem.sentence(word_count: (50..100).to_a.sample),
+               gender: ["M","F","Other"].sample,
+               city: Faker::Address.city)
+  user.save!
+  (0..2).to_a.sample.times do
+    puts "adding specialty"
+    specialty = Specialty.create!(title: Faker::Hobby.activity,
+                                  details: Faker::Lorem.sentence(word_count: (20..50).to_a.sample),
+                                  price: (0..100).to_a.sample.to_i,
+                                  user: user )
+    specialty.save!
+    (0..1).to_a.sample.times do
+      puts "creating reservations"
+      reservation = Reservation.create!(date: DateTime.now + (1..30).to_a.sample.days,
+                                      comment: Faker::Lorem.sentence(word_count: (10..20).to_a.sample),
+                                      specialty: specialty,
+                                      user: user_resa,
+                                      is_accepted: [nil, true, false].sample)
+      reservation.save!
+    end
+  end
+  counter += 1
+end
+
+puts "Database ready"
