f32c88c017c81ecd1bfac88d89ae58edc8341972
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2022-08-22 15:48:21 +0200

    device finished setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :birthdate, :description, :gender, :city) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :birthdate, :description, :gender, :city) }
+  end
 end
