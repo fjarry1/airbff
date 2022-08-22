93011cc0336a601500d6c43a4aa03e7ac4382c0f
Francois Jarry <francoisjarry1@gmail.com>
2022-08-22 16:15:48 +0200

    fixed devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :birthdate, :description, :gender, :city) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :birthdate, :description, :gender, :city) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :description, :gender, :city])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthdate, :description, :gender, :city])
   end
 end
