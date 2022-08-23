7f672c3719237c4643bf4346df0d53ecf0abe07a
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2022-08-23 15:40:33 +0200

    profil incoming

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,7 +46,7 @@ counter = 1
   file = URI.open("https://source.unsplash.com/random/?profile")
   user.photo.attach(io: file, filename: "avatar#{counter}.png", content_type: "image/png")
   user.save!
-  (0..2).to_a.sample.times do
+  (0..5).to_a.sample.times do
     puts "adding specialty"
     specialty = Specialty.create!(title: Faker::Hobby.activity,
                                   details: Faker::Lorem.sentence(word_count: (20..50).to_a.sample),
