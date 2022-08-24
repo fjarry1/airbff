6575910814782bbe21e88b83b36e7c65b651278a
christelle bettoni <christelle.bettoni@gmail.com>
2022-08-24 11:05:01 +0200

    lien specialties cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @specialties = Specialty.all
+    @specialties = Specialty.first(8)
   end
 
   def profil
